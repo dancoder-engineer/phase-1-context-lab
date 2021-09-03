5176047c7b60d99f8a532e1311ef64aea1f07328
Dan Seminara <japaneseforgaming@gmail.com>
2021-09-02 22:29:21 -0400

    commit tree 0576cf8ba574dd5741b010d3c608ef7f7f38b4a8

--- index.js
+++ index.js
@@ -21,3 +21,78 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr) {
+        return arr.map(creat)
+}
+
+function creat(i) {
+    return createEmployeeRecord(i)
+}
+
+
+
+function createTimeInEvent(stamp) {
+    let returnObj = {
+        type: "TimeIn",
+        hour: parseInt(stamp.slice(11,)),
+        date: stamp.slice(0,10)
+    }
+
+    this.timeInEvents.push(returnObj)
+
+    return this
+}
+
+function createTimeOutEvent(stamp) {
+
+    let returnObj = {
+        type: "TimeOut",
+        hour: parseInt(stamp.slice(11,)),
+        date: stamp.slice(0,10)
+    }
+
+    this.timeOutEvents.push(returnObj)
+
+    //console.log(this)
+    return this
+}
+
+function hoursWorkedOnDate(stamp) {
+    let start = this.timeInEvents.find(i => stamp === i.date)
+    let finish = this.timeOutEvents.find(i => stamp === i.date)
+    return (finish.hour-start.hour)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName (employees, name) {
+    let c =  employees.find(function(i) {
+        
+        return i.firstName === name
+    })
+    return c
+}
+
+function calculatePayroll(arr) {
+    return arr.reduce(addUp, 0)
+}
+
+function addUp (acc, i) {
+    return acc + allWagesFor.call(i)
+}
+
